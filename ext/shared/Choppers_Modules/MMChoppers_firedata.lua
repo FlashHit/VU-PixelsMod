f14a3aa9ed0ad2c0daaea5d3c874111b2f596f6d
Ton Muller <spatieman@gmail.com>
2022-04-05 11:06:02 +0200

    Add files via upload

--- ext/shared/Choppers_Modules/MMChoppers_firedata.lua
+++ ext/shared/Choppers_Modules/MMChoppers_firedata.lua
@@ -1,7 +1,7 @@
 class "MMChoppers_firedata"
 
 function MMChoppers_firedata:Write(mmResources)
-		dprint('Loading MMChoppers_firedat module ... ... ... ... ... ')
+--		dprint('Loading MMChoppers_firedat module ... ... ... ... ... ')
 
 
 	if (mmResources:IsLoaded('rocketpods')) then
@@ -11,7 +11,8 @@ function MMChoppers_firedata:Write(mmResources)
 		local fireData = FiringFunctionData(mmResources:GetInstance('rocketpods'))
 		fireData:MakeWritable()
 --		fireData.fireLogic.rateOfFire = fireData.fireLogic.rateOfFire + 300
-		fireData.ammo.magazineCapacity = 50
+--		fireData.reloadTime = 5.0
+		fireData.ammo.magazineCapacity = 45
 		fireData.ammo.numberOfMagazines = -1
 	
 		dprint('Changed Atack Chopper - Super charched Rocketpods ammo... ... ... ... ... ')
@@ -21,22 +22,24 @@ function MMChoppers_firedata:Write(mmResources)
 		dprint('Loading Chopper Gunner data ... ... ... ... ... ')
 		mmResources:SetLoaded('gunnercannon', false)
 
-		local bulletData = BulletEntityData(mmResources:GetInstance('gunnercannon'))
+--		local bulletData = BulletEntityData(mmResources:GetInstance('gunnercannon'))
+		local bulletData = FiringFunctionData(mmResources:GetInstance('gunnercannon'))
 		bulletData:MakeWritable()
 
-		bulletData.initialSpeed = 400
+--		bulletData.initialSpeed = 400
 --		bulletData.gravity = 0
-		bulletData.startDamage = 25
-		bulletData.endDamage = 25
-		bulletData.damageFalloffStartDistance = 100
-		bulletData.damageFalloffEndDistance = 300
-		bulletData.timeToLive = 15
-		bulletData.impactImpulse = 25
+--		bulletData.startDamage = 25
+--		bulletData.endDamage = 25
+--		bulletData.damageFalloffStartDistance = 100
+--		bulletData.damageFalloffEndDistance = 300
+--		bulletData.timeToLive = 15
+--		bulletData.impactImpulse = 25
+--		bulletData.reloadTime = 2.0
 
-		bulletData.numberOfBulletsPerShell = 1
-		bulletData.numberOfBulletsPerShot = 1
-		bulletData.numberOfBulletsPerBurst = 3
-		bulletData.ammo.magazineCapacity = 50
+--		bulletData.numberOfBulletsPerShell = 1
+--		bulletData.numberOfBulletsPerShot = 1
+--		bulletData.numberOfBulletsPerBurst = 3
+		bulletData.ammo.magazineCapacity = 90
 		bulletData.ammo.numberOfMagazines = -1
 		dprint('Changed Atack Chopper - Gunner bullet entity ammo... ... ... ... ... ')
 	end
--- ext/shared/Common_Weapons_Modules/MMWeapons_AS_val.lua
+++ ext/shared/Common_Weapons_Modules/MMWeapons_AS_val.lua
@@ -5,13 +5,12 @@ function MMWeapons_AS_val:Write(mmResources)
 	if (mmResources:IsLoaded('asval')) then
 		mmResources:SetLoaded('asval', false)
 
-		local weaponBP = SoldierWeaponBlueprint(mmResources:GetInstance('asval'))
-		local weaponData = SoldierWeaponData(weaponBP.object)
-		local fireData = ebxEditUtils:GetWritableContainer(weaponData, 'weaponFiring.primaryFire')
+		local fireData = FiringFunctionData(mmResources:GetInstance('asval'))
+		fireData:MakeWritable()
 		fireData.fireLogic.rateOfFire = fireData.fireLogic.rateOfFire + 300
 		fireData.ammo.magazineCapacity = fireData.ammo.magazineCapacity * 3
-		fireData.ammo.numberOfMagazines = 10
-		dprint('Changed AS-Val - Pimped AMMO size...')
+--		fireData.ammo.numberOfMagazines = 10
+		dprint('Changed AS-Val - (mod 2) Pimped AMMO size...')
 	end
 
 end
