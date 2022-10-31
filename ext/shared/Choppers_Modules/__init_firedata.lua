3ddbe5f1a485cf26f9cea54c94764a5838b62bfa
Ton Muller <spatieman@gmail.com>
2022-10-31 17:48:13 +0100

    Add files via upload

--- ext/shared/Choppers_Modules/__init_firedata.lua
+++ ext/shared/Choppers_Modules/__init_firedata.lua
@@ -1,12 +1,6 @@
-   local partition_litlebird = Guid ("5490C4AB-A322-3F65-9680-CF9CD9D32705")
-   local instance_litlebird = Guid ("FB438F0C-EB84-A96A-4836-F6C624FE21E0")
-
+--
    local partition_rocketpods = Guid ("E64E52BD-4E40-4BFE-B6C3-49523084AE94")
    local instance_rocketpods = Guid ("3EE6789F-4113-42BA-B8FE-79DD5EF89FB1")
-   local partition_gunnercannon = Guid ("0A232494-FB58-496E-BF30-FD9B39E12008")
-   local instance_gunnercannon = Guid ("E06B6CCE-F5A3-40E6-A41F-D442AC4D7434")
-   local partition_gunnercannon1 = Guid ("0D61B717-18D3-4295-9070-9AB925B1C784")
-   local instance_gunnercannon1 = Guid ("CAB59337-47AF-4742-9CEC-3EEA7EA20E92")
 
 ResourceManager:RegisterInstanceLoadHandler(partition_rocketpods, instance_rocketpods, function(loadedInstance)
      loadedInstance = FiringFunctionData(loadedInstance)
@@ -14,8 +8,12 @@ ResourceManager:RegisterInstanceLoadHandler(partition_rocketpods, instance_rocke
 
     loadedInstance.ammo.magazineCapacity = 45
     loadedInstance.ammo.numberOfMagazines = -1
-	print('Changed Chopper Firedata: Atack Choppers - Increased rocketpods ... ... ... ... ... ')
+	print('Changed Chopper Firedata: US/RU Atack Choppers - Increased rocketpods ...')
 	end)
+
+--
+   local partition_gunnercannon = Guid ("0A232494-FB58-496E-BF30-FD9B39E12008")
+   local instance_gunnercannon = Guid ("E06B6CCE-F5A3-40E6-A41F-D442AC4D7434")
 
 ResourceManager:RegisterInstanceLoadHandler(partition_gunnercannon, instance_gunnercannon, function(loadedInstance)
      loadedInstance = FiringFunctionData(loadedInstance)
@@ -23,8 +21,12 @@ ResourceManager:RegisterInstanceLoadHandler(partition_gunnercannon, instance_gun
 
     loadedInstance.ammo.magazineCapacity = 90
     loadedInstance.ammo.numberOfMagazines = -1
-	print('Changed Chopper Firedata: US Atack Chopper - Gunner bullet entity ammo... ... ... ... ... ')
+	print('Changed Chopper Firedata: US Atack Chopper - Gunner bullet entity ammo ...')
 	end)
+
+--
+   local partition_gunnercannon1 = Guid ("0D61B717-18D3-4295-9070-9AB925B1C784")
+   local instance_gunnercannon1 = Guid ("CAB59337-47AF-4742-9CEC-3EEA7EA20E92")
 
 ResourceManager:RegisterInstanceLoadHandler(partition_gunnercannon1, instance_gunnercannon1, function(loadedInstance)
      loadedInstance = FiringFunctionData(loadedInstance)
@@ -32,8 +34,12 @@ ResourceManager:RegisterInstanceLoadHandler(partition_gunnercannon1, instance_gu
 
     loadedInstance.ammo.magazineCapacity = 90
     loadedInstance.ammo.numberOfMagazines = -1
-	print('Changed Chopper Firedata: RU Atack Chopper - Gunner bullet entity ammo... ... ... ... ... ')
+	print('Changed Chopper Firedata: RU Atack Chopper - Gunner bullet entity ammo ...')
 	end)
+
+--
+   local partition_litlebird = Guid ("5490C4AB-A322-3F65-9680-CF9CD9D32705")
+   local instance_litlebird = Guid ("FB438F0C-EB84-A96A-4836-F6C624FE21E0")
 
 ResourceManager:RegisterInstanceLoadHandler(partition_litlebird, instance_litlebird, function(loadedInstance)
      loadedInstance = FiringFunctionData(loadedInstance)
@@ -41,6 +47,72 @@ ResourceManager:RegisterInstanceLoadHandler(partition_litlebird, instance_litleb
 
     loadedInstance.ammo.magazineCapacity = 1000
     loadedInstance.ammo.numberOfMagazines = -1
-	print('Changed Chopper Firedata: US/RU  Light Atack Chopper - Gunner bullet entity ammo... ... ... ... ... ')
+	print('Changed Chopper Firedata: US/RU  Light Atack Chopper - Gunner bullet entity ammo ...')
 	end)
+
+--
+
+   local partition_lav25_atgm = Guid ("ACFD0C2A-2D69-496A-AE55-9E09EE43BAF0")
+   local instance_lav25_atgm = Guid ("538CDF32-5BBF-4D0B-849D-A59079D0AEEA")
+
+ResourceManager:RegisterInstanceLoadHandler(partition_lav25_atgm, instance_lav25_atgm, function(loadedInstance)
+     loadedInstance = FiringFunctionData(loadedInstance)
+    loadedInstance:MakeWritable()
+
+    loadedInstance.ammo.magazineCapacity = 12
+    loadedInstance.ammo.numberOfMagazines = -1
+    loadedInstance.fireLogic.reloadTime = 4.0
+	print('Changed Atack Vehicle Firedata : US LAV-25 ATGM Launcher ...')
+	end)
+
+--
+
+   local partition_bmp2m_atgm = Guid ("9A3C60F7-4A0F-B735-3B7C-FA64DDE35E4E")
+   local instance_bmp2m_atgm = Guid ("52EC2FFF-2E86-26DC-41A2-4CA3B92F3C05")
+
+ResourceManager:RegisterInstanceLoadHandler(partition_bmp2m_atgm, instance_bmp2m_atgm, function(loadedInstance)
+     loadedInstance = FiringFunctionData(loadedInstance)
+    loadedInstance:MakeWritable()
+
+    loadedInstance.ammo.magazineCapacity = 12
+    loadedInstance.ammo.numberOfMagazines = -1
+    loadedInstance.fireLogic.reloadTime = 4.0
+	print('Changed Atack Vehicle Firedata : RU BMP-2M ATGM Launcher ...')
+	end)
+-- IFV_ATGM_Projectile lav25 btr90 m142 bm32
+
+-- RU SU-35BM
+-- ---------------------------------------------------------------------------------------
+-- Heatseakers on US Atack chopper and US Jet FA18E increased
+
+
+   local partition_viper_heatseaker = Guid ("085E96E4-5C8C-4A79-A819-1D4B0B4137E7") -- US
+   local instance_viper_heatseaker = Guid ("63E6F602-7274-4AB8-8CFB-E2C00EAF2A73") -- US
+
+ResourceManager:RegisterInstanceLoadHandler(partition_viper_heatseaker, instance_viper_heatseaker, function(loadedInstance)
+     loadedInstance = FiringFunctionData(loadedInstance)
+    loadedInstance:MakeWritable()
+
+    loadedInstance.ammo.magazineCapacity = 6
+    loadedInstance.ammo.numberOfMagazines = -1
+
+    loadedInstance.fireLogic.reloadTime = 4.0
+	print('Changed Chopper Firedata : US Atack Chopper / FA18E - Increased Heatseakers ...')
+	end)
+
+-- ----------------------------------------
+   local partition_mi28_heatseaker = Guid ("34EFA6A0-D3EE-4219-BE3F-7F0DC993E1B7") -- 
+   local instance_mi28_heatseaker = Guid ("59B9B446-3866-47FC-8B97-5DFFF64945AA") --
+
+ResourceManager:RegisterInstanceLoadHandler(partition_mi28_heatseaker, instance_mi28_heatseaker, function(loadedInstance)
+     loadedInstance = FiringFunctionData(loadedInstance)
+    loadedInstance:MakeWritable()
+
+    loadedInstance.ammo.magazineCapacity = 6
+    loadedInstance.ammo.numberOfMagazines = -1
+
+    loadedInstance.fireLogic.reloadTime = 4.0
+	print('Changed UNKNOWN Firedata : ??????????????????????????????????? ...')
+	end)
+-- ----------------------------------------
 
