a440677b99aca0c656ae2c7f2690fc4a7bbf61c5
Ton Muller <spatieman@gmail.com>
2022-11-12 14:01:49 +0100

    Add files via upload

--- ext/shared/__init_modules_shotguns.lua
+++ ext/shared/__init_modules_shotguns.lua
@@ -7,6 +7,7 @@ local bEnable_modules_shotguns = true
 
 
 -- Shotgun stuff combined --
+ bEnable_firedata = true
  bEnable_spas12 = true
  bEnable_MK3A1 = true
  bEnable_Remington870MCS = true
@@ -19,6 +20,7 @@ local bEnable_modules_shotguns = true
 	    if (bEnable_modules_shotguns) == (false) then print('Shotgun Modules config Disabled... ... ...')
 
 -- Shotgun stuff combined --
+ bEnable_firedata = false
  bEnable_spas12 = false
  bEnable_MK3A1 = false
  bEnable_Remington870MCS = false
@@ -31,10 +33,20 @@ local bEnable_modules_shotguns = true
 end
 
 -- ---------------------------------------------------------------------
+-- Shotgun tweaked firedata --
+
+            if (bEnable_firedata) == (true) then print('Shotgun tweaked firedata Enabled...')
+        __init_Shotgun_firedata = require('__shared/Shotgun_Modules/__init_Shotgun_firedata')
+	else
+	    if (bEnable_firedata) == (false) then print('Shotgun tweaked firedata Disabled...')
+end
+end
+--
+
+
 -- Weapons Shotguns combined modules --
             if (bEnable_DAO12) == (true) then print('DAO12 Enabled...')
         __init_Shotgun_DAO12 = require('__shared/Shotgun_Modules/__init_Shotgun_DAO12')
--- mmResources:AddLoadHandler(__init_Shotgun_DAO12, __init_Shotgun_DAO12.Write)
 	else
 	    if (bEnable_DAO12) == (false) then print('DAO12 Disabled...')
 end
@@ -43,7 +55,6 @@ end
 
             if (bEnable_M1014) == (true) then print('M1014 Enabled...')
         __init_Shotgun_M1014 = require('__shared/Shotgun_Modules/__init_Shotgun_M1014')
--- mmResources:AddLoadHandler(__init_Shotgun_M1014, __init_Shotgun_M1014.Write)
 	else
 	    if (bEnable_M1014) == (false) then print('M1014 Disabled...')
 end
@@ -52,7 +63,6 @@ end
 
             if (bEnable_USAS12) == (true) then print('USAS12 Enabled...')
         __init_Shotgun_USAS12 = require('__shared/Shotgun_Modules/__init_Shotgun_USAS12')
--- mmResources:AddLoadHandler(__init_Shotgun_USAS12, __init_Shotgun_USAS12.Write)
 	else
 	    if (bEnable_USAS12) == (false) then print('USAS12 Disabled...')
 end
@@ -61,7 +71,6 @@ end
 
             if (bEnable_Saiga20K) == (true) then print('Saiga20K Enabled...')
         __init_Shotgun_Saiga20K = require('__shared/Shotgun_Modules/__init_Shotgun_Saiga20K')
--- mmResources:AddLoadHandler(__init_Shotgun_Saiga20K, __init_Shotgun_Saiga20K.Write)
 	else
 	    if (bEnable_Saiga20K) == (false) then print('Saiga20K Disabled...')
 end
@@ -70,7 +79,6 @@ end
 
             if (bEnable_Remington870MCS) == (true) then print('Remington 870MCS Enabled...')
         __init_Shotgun_Remington870MCS = require('__shared/Shotgun_Modules/__init_Shotgun_Remington870MCS')
--- mmResources:AddLoadHandler(__init_Shotgun_Remington870MCS, __init_Shotgun_Remington870MCS.Write)
 	else
 	    if (bEnable_Remington870MCS) == (false) then print('Remington 870MCS Disabled...')
 end
@@ -79,7 +87,6 @@ end
 
 	    if (bEnable_spas12) == (true) then print('spas12 Enabled...')
         __init_Shotgun_spas12 = require('__shared/Shotgun_Modules/__init_Shotgun_spas12')
--- mmResources:AddLoadHandler(__init_Shotgun_spas12, __init_Shotgun_spas12.Write)
 	else
 	    if (bEnable_spas12) == (false) then print('spas12 Disabled...')
 end
@@ -88,7 +95,6 @@ end
 
 	    if (bEnable_MK3A1) == (true) then print('MK3A1 Jackhammer Enabled...')
         __init_Shotgun_MK3A1 = require('__shared/Shotgun_Modules/__init_Shotgun_MK3A1')
--- mmResources:AddLoadHandler(__init_Shotgun_MK3A1, __init_Shotgun_MK3A1.Write)
 	else
 	    if (bEnable_MK3A1) == (false) then print('MK3A1 Jackhammer Disabled...')
 end
