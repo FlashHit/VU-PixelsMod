c7a7044b97945ae8ad9def92371e40684b915c2c
Ton Muller <spatieman@gmail.com>
2022-11-14 18:34:25 +0100

    Add files via upload

--- ext/shared/Support_Weapons_Modules/__init_support_no_recoil.lua
+++ ext/shared/Support_Weapons_Modules/__init_support_no_recoil.lua
@@ -249,3 +249,28 @@ Events:Subscribe('Partition:Loaded', function(partition)
  end)
 
 -- ################
+
+Events:Subscribe('Partition:Loaded', function(partition)
+	local instances = partition.instances
+
+	for _, instance in pairs(instances) do
+		-- Check if the instance is of the type we are looking for.
+		if instance.instanceGuid == Guid('3AA993A0-5A1F-4BE6-8D03-B8018815B93E') then
+			-- Cast it so we can access its fields.
+			local gunSwayData = GunSwayData(instance)
+			-- Make it writable so we can modify its fields.
+			gunSwayData:MakeWritable()
+
+			gunSwayData.deviationScaleFactorZoom = 0
+			gunSwayData.gameplayDeviationScaleFactorZoom = 0
+			gunSwayData.deviationScaleFactorNoZoom = 0
+			gunSwayData.gameplayDeviationScaleFactorNoZoom = 0
+			gunSwayData.shootingRecoilDecreaseScale = 0
+			gunSwayData.firstShotRecoilMultiplier = 0
+    dprint("Support: Removing recoil from MG36 ...") 
+
+    end
+   end
+ end)
+
+-- ################
