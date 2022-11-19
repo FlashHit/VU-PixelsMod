1b17bb491c9e0ef8e5613afda73202a4073d77e1
Ton Muller <spatieman@gmail.com>
2022-11-19 12:27:38 +0100

    Add files via upload

--- ext/shared/Gadgets_Modules/__init_Tugs.lua
+++ ext/shared/Gadgets_Modules/__init_Tugs.lua
@@ -19,30 +19,30 @@ end)
 
 --
 
-	local partition_more_tugs = Guid ("483F1918-1815-11E0-8BA5-9B1E2E41035E")
-         local Instance_more_tugs = Guid ("974BC154-2239-4619-B845-271BEBAFE33A")
-
- ResourceManager:RegisterInstanceLoadHandler(partition_more_tugs, Instance_more_tugs, function(loadedInstance)
-
-    loadedInstance = FiringFunctionData(loadedInstance)
-    loadedInstance:MakeWritable()
-
-    loadedInstance.ammo.numberOfMagazines = 5 -- Default = 1
-    loadedInstance.ammo.magazineCapacity = 1 -- Default = 1
-	print('Changed Gadgets: T-UGS - Can hold More to place ! ...')
- end)
-
---
-	local partition_placemore_tugs = Guid ("483F1918-1815-11E0-8BA5-9B1E2E41035E")
-         local Instance_placemore_tugs = Guid ("E8BDE781-EBCA-4FFB-8219-6DCAC05DE5AE")
-
-ResourceManager:RegisterInstanceLoadHandler(partition_placemore_tugs, Instance_placemore_tugs, function(loadedInstance)
-
-    loadedInstance = VehicleProjectileEntityData(loadedInstance)
-    loadedInstance:MakeWritable()
-
-    loadedInstance.maxCount = 5
-	print('Changed Gadgets: T-UGS - can place more ! ...')
-	end)
-
---
+-- 	local partition_more_tugs = Guid ("483F1918-1815-11E0-8BA5-9B1E2E41035E")
+--          local Instance_more_tugs = Guid ("974BC154-2239-4619-B845-271BEBAFE33A")
+-- 
+--  ResourceManager:RegisterInstanceLoadHandler(partition_more_tugs, Instance_more_tugs, function(loadedInstance)
+-- 
+--     loadedInstance = FiringFunctionData(loadedInstance)
+--     loadedInstance:MakeWritable()
+-- 
+--     loadedInstance.ammo.numberOfMagazines = 5 -- Default = 1
+--     loadedInstance.ammo.magazineCapacity = 1 -- Default = 1
+-- 	print('Changed Gadgets: T-UGS - Can hold More to place ! ...')
+--  end)
+-- 
+-- --
+-- 	local partition_placemore_tugs = Guid ("483F1918-1815-11E0-8BA5-9B1E2E41035E")
+--          local Instance_placemore_tugs = Guid ("E8BDE781-EBCA-4FFB-8219-6DCAC05DE5AE")
+-- 
+-- ResourceManager:RegisterInstanceLoadHandler(partition_placemore_tugs, Instance_placemore_tugs, function(loadedInstance)
+-- 
+--     loadedInstance = VehicleProjectileEntityData(loadedInstance)
+--     loadedInstance:MakeWritable()
+-- 
+--     loadedInstance.maxCount = 5
+-- 	print('Changed Gadgets: T-UGS - can place more ! ...')
+-- 	end)
+-- 
+-- --
