9b54fb9db24868392a2584821e0fd43f915bc9d1
Ton Muller <spatieman@gmail.com>
2022-11-23 09:11:35 +0100

    Add files via upload

--- ext/shared/Choppers_Modules/__init_firedata.lua
+++ ext/shared/Choppers_Modules/__init_firedata.lua
@@ -104,7 +104,7 @@ ResourceManager:RegisterInstanceLoadHandler(partition_litlebird, instance_litleb
      loadedInstance.fireLogic.reloadTime = 4.0
  	print('Changed Chopper Firedata : RU Atack Chopper - Increased Heatseakers ...')
  	end)
--- ----------------------------------------
+
 -- ----------------------------------------
 -- Heatseakers on RU Jet increased
 
@@ -121,3 +121,63 @@ ResourceManager:RegisterInstanceLoadHandler(partition_litlebird, instance_litleb
      loadedInstance.fireLogic.reloadTime = 4.0
  	print('Changed jet Firedata : RU jet - Increased Heatseakers ...')
  	end)
+
+
+-- ----------------------------------------
+-- Gunship tweaks
+
+    local partition_gunship_ac = Guid ("D1507C85-243B-45D5-9378-9ADE0C936909") -- 
+     local instance_gunship_ac = Guid ("1EBA0568-FB66-4BCA-BC5D-D2C43FD0ECF6") --
+ 
+ ResourceManager:RegisterInstanceLoadHandler(partition_gunship_ac, instance_gunship_ac, function(loadedInstance)
+     loadedInstance = FiringFunctionData(loadedInstance)
+     loadedInstance:MakeWritable()
+ 
+     loadedInstance.ammo.magazineCapacity = 90 -- Default = 30
+ 
+     loadedInstance.fireLogic.reloadTime = 4.0
+ 	print('Changed Gunship AutoCannon ammo - Increased ammo ...')
+ 	end)
+
+-- ----------------------------------------
+
+   local partition_gunship_ac_dmg = Guid ("D1507C85-243B-45D5-9378-9ADE0C936909")
+    local instance_gunship_ac_dmg = Guid ("18003E66-D168-4E53-BCDD-BCAB3FEF8419")
+
+ ResourceManager:RegisterInstanceLoadHandler(partition_gunship_ac_dmg, instance_gunship_ac_dmg, function(loadedInstance)
+      loadedInstance = VeniceExplosionEntityData(loadedInstance)
+     loadedInstance:MakeWritable()
+ 
+     loadedInstance.blastDamage = 120 -- Normal = 35
+     loadedInstance.blastRadius = 9 -- Normal = 3
+     loadedInstance.blastImpulse = 8000 -- Normal = 5000
+     loadedInstance.shockwaveDamage = 8 -- Normal = 1
+     loadedInstance.shockwaveRadius = 15 -- Normal = 7
+     loadedInstance.shockwaveImpulse = 30 -- Normal = 10
+     loadedInstance.shockwaveTime = 0.1 -- Deafault = 0.5
+     loadedInstance.triggerImpairedHearing = true
+     loadedInstance.isCausingSuppression = true
+ 	print('Changed Gunship AutoCannon ammo - Increased Damage ...')
+ 	end) 
+
+-- ----------------------------------------
+
+   local partition_gunship_60mm_dmg = Guid ("0D139F47-C360-428C-A91B-5B247F236F0F")
+    local instance_gunship_60mm_dmg = Guid ("5A903893-799A-4B52-8A5C-6AE373D7038C")
+
+ ResourceManager:RegisterInstanceLoadHandler(partition_gunship_60mm_dmg, instance_gunship_60mm_dmg, function(loadedInstance)
+      loadedInstance = VeniceExplosionEntityData(loadedInstance)
+     loadedInstance:MakeWritable()
+ 
+     loadedInstance.blastDamage = 600 -- Normal = 150
+     loadedInstance.blastRadius = 40 -- Normal = 10
+     loadedInstance.blastImpulse = 10000 -- Normal = 5000
+     loadedInstance.shockwaveDamage = 50 -- Normal = 1
+     loadedInstance.shockwaveRadius = 60 -- Normal = 15
+     loadedInstance.shockwaveImpulse = 5000 -- Normal = 2500
+     loadedInstance.shockwaveTime = 0.1 -- Deafault = 0.14
+     loadedInstance.triggerImpairedHearing = true
+     loadedInstance.isCausingSuppression = true
+ 	print('Changed Gunship 60mm cannon - Increased Damage ...')
+ 	end) 
+
