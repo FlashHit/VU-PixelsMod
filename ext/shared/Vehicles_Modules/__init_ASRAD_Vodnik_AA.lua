08961fffbc354c6ad43b1bb80abb41757f1c26fc
Ton Muller <spatieman@gmail.com>
2022-11-25 11:20:16 +0100

    Add files via upload

--- ext/shared/Vehicles_Modules/__init_ASRAD_Vodnik_AA.lua
+++ ext/shared/Vehicles_Modules/__init_ASRAD_Vodnik_AA.lua
@@ -8,6 +8,22 @@ ResourceManager:RegisterInstanceLoadHandler(partition_asrad_aa, instance_asrad_a
     loadedInstance:MakeWritable()
 
     loadedInstance.ammo.magazineCapacity = 24
-		print('Changed ASRAD / Vodnik Shell Modifier ... ... ... ... ... ...')
+	print('Changed ASRAD / Vodnik Shell Modifier ...')
 	end)
 
+-- ---------------------
+
+    local partition_asrad_grav = Guid ("4CBA81C7-2078-4B37-BF1B-8D03C7783363")
+     local instance_asrad_grav = Guid ("637A87DE-FC71-412A-98AC-522FA7005BB6") 
+
+ResourceManager:RegisterInstanceLoadHandler(partition_asrad_grav, instance_asrad_grav, function(loadedInstance)
+     loadedInstance = MissileEntityData(loadedInstance)
+    loadedInstance:MakeWritable()
+
+    loadedInstance.engineStrength = 150 -- default = 100.0
+    loadedInstance.maxSpeed = 250       -- default = 150.0
+    loadedInstance.initialSpeed = 700 -- default = 350
+    loadedInstance.gravity = 0        -- default = -1.5
+    loadedInstance.timeToLive = 8     -- default = 10
+	print('Changed ASRAD / Vodnik No gravity on shell ...')
+	end) 
--- ext/shared/Vehicles_Modules/__init_tank_lbt_phoenix_barsuk.lua
+++ ext/shared/Vehicles_Modules/__init_tank_lbt_phoenix_barsuk.lua
@@ -6,5 +6,40 @@ ResourceManager:RegisterInstanceLoadHandler(partition_lbt_shell4, instance_lbt_s
     loadedInstance:MakeWritable()
 	
     loadedInstance.ammo.magazineCapacity = 24
-	print('Changed LBT Phoenix - Barsuk Shell Modifier ... ... ... ... ... ...')
+	print('Changed LBT Phoenix - Barsuk Shell ammo Modifier ...')
 	end)
+
+-- -----------------------
+    local partition_phnx_bar_grav = Guid ("E8E52039-27D8-4629-8E4D-7B1F8A419175")
+     local instance_phnx_bar_grav = Guid ("E774DD5A-F94C-4CD3-98B4-72C072679B59") 
+
+ResourceManager:RegisterInstanceLoadHandler(partition_phnx_bar_grav, instance_phnx_bar_grav, function(loadedInstance)
+     loadedInstance = BulletEntityData(loadedInstance)
+    loadedInstance:MakeWritable()
+
+    loadedInstance.initialSpeed = 450 -- default = 350
+    loadedInstance.gravity = 0        -- default = -7
+    loadedInstance.timeToLive = 6     -- default = 5
+	print('Changed LBT Phoenix - Barsuk Shell Gravity Modifier ...')
+	end) 
+
+-- -----------------------
+
+   local partition_phnx_bar_dmg = Guid ("E8E52039-27D8-4629-8E4D-7B1F8A419175")
+    local instance_phnx_bar_dmg = Guid ("319579A1-8F5E-4B6F-AF55-81F92BBAA56E")
+
+ ResourceManager:RegisterInstanceLoadHandler(partition_phnx_bar_dmg, instance_phnx_bar_dmg, function(loadedInstance)
+      loadedInstance = VeniceExplosionEntityData(loadedInstance)
+     loadedInstance:MakeWritable()
+ 
+     loadedInstance.blastDamage = 75 -- Normal = 34
+     loadedInstance.blastRadius = 10 -- Normal = 5
+     loadedInstance.blastImpulse = 1000 -- Normal = 750
+     loadedInstance.shockwaveDamage = 10 -- Normal = 1
+     loadedInstance.shockwaveRadius = 16 -- Normal = 8
+     loadedInstance.shockwaveImpulse = 80 -- Normal = 40
+     loadedInstance.shockwaveTime = 0
+     loadedInstance.triggerImpairedHearing = true
+     loadedInstance.isCausingSuppression = true
+ 	print('Changed LBT Phoenix - Barsuk Shell explosion modifier ...')
+ 	end) 
