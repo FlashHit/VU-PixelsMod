7e4bd4f84a4733cd22cc8c3e0aa03b0f7b020cfe
Ton Muller <spatieman@gmail.com>
2022-12-03 09:47:33 +0100

    Update version.lua

--- ext/shared/version.lua
+++ ext/shared/version.lua
@@ -1,3 +1,4 @@
-localModVersion = "2.1.4";
+localModVersion = "2.1.5";
 -- current version for mod.json
--- do not forgot to update mod.json and version.lua if u sync with github !
+-- do not forgot to update mod.json if u sync with github !
+-- and edit version.lua directly on github xD
